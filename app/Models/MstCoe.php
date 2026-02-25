58d269852ecf0ad17ba415cf0f8369727fe7da4a
jethroGIT <jethro.andersson02@gmail.com>
2026-02-25 12:42:56 +0700

    fix: rename

--- app/Models/MstCoe.php
+++ app/Models/MstCoe.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class TrsCoe extends Model
+class MstCoe extends Model
 {
     protected $table = 'mst_coe';
 
